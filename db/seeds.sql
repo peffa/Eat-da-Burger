bdd1fc771820268eba28fbc0cab45705ca5af591
Ryan Peffer <ryanpeffer@icloud.com>
2020-06-30 17:46:49 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
